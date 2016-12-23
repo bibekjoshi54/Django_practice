1f05881bdfcd595f3503063e320b027a6530fc9a
Bibek Joshi <bibekjoshi54@gmail.com>
2016-12-23 14:56:13 +0530

    Submission from and class views used

--- website/music/models.py
+++ website/music/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
-
+#check
 class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
