de76c96979f83e3abb5b069333a08c277447ab12
Bibek Joshi <bibekjoshi54@gmail.com>
2016-12-23 14:55:31 +0530

    Submission from and class views used

--- website/music/models.py
+++ website/music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Album(models.Model):
@@ -7,8 +8,11 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def get_absolute_url(self):
+        return reverse("music:detail", kwargs={'pk': self.pk})
+
     def __str__(self):
-        return (self.album_title + '-' +self.artist + '-'+ self.genre)
+        return self.album_title + '-' +self.artist + '-'+ self.genre
 
 
 class Song(models.Model):
@@ -16,7 +20,6 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
     is_favorite = models.BooleanField(default=False)
-
 
     def __str__(self):
         return self.song_title
